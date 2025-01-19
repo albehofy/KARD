225144e1606d24a8272896cada41fe0bdf70ca6f
Mohamed Reda <moreda272@gmail.com>
2025-01-19 11:11:29 +0200

    Intializing Layouts

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonModule, InputTextModule],
+  imports: [ButtonModule, InputTextModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './Components/login/login.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { SocialMediaComponent } from './Pages/social-media/social-media.component';
 
 export const routes: Routes = [
     {path: '',component: LoginComponent},
+    {path: 'home',component: HomeComponent, children: [
+       {
+        path: 'socialMedia', component: SocialMediaComponent
+       }
+    ]},
 ];
